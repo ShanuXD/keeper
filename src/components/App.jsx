20c2d286edee7c2afe0c3c9eef72d4c2f644ddb5
shantanu <shanu09.sm@gmail.com>
2020-08-30 22:49:21 +0530

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from './Heading'
-import Note from './Note'
 import Footer from './Footer'
-import notes from '../notes'
-/*
-function createNote(item){
-    return <Note 
-    key={item.key}
-    title={item.title}
-    discription={item.content}
-    />
+import Note from './Note'
+import CreateArea from './CreateArea'
+
+function App() {
+
+    const [notes,setNotes]=useState([{title: 'note',
+    content: 'Hello World'}])
+
+function addNote(newNote){
+setNotes(prevNotes=>{
+   return [...prevNotes,newNote]
+})
 }
-*/
-function App(){
+
+function deleteNote(id){
+setNotes(prevNotes => {
+    return prevNotes.filter((noteItem,index)=>{
+        return index !== id
+    })
+})
+}
+
     return (
-    <div>
-    <Header />
-    {notes.map((item)=> <Note 
-    key={item.key}
-    title={item.title}
-    discription={item.content}
-    />
-    )}
-    <Footer />
-    </div>)
-}
+      <div>
+        <Header />
+        <CreateArea
+        onAdd={addNote} />
+        { notes.map((noteItem,index)=>{
+            return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+        })}
+        <Footer />
+      </div>
+    );
+  }
+  
 
 export default App
